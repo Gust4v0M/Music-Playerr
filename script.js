a0f40a637fdec06892391516e1d6ee63f7e1b5b2
Gust4v0M <gustavomorgado6@gmail.com>
2022-04-15 23:40:35 -0300

    mumu version

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let music = document.getElementById("music")
 let barra = document.getElementById("barra")
 let ponto = document.getElementById("ponto")
+let container = document.getElementById("container")
 let barraProg = document.getElementById("barraProg")
 
 let tempoAtual = document.getElementById("tempo_atual")
@@ -9,19 +10,16 @@ let tempoTotal = document.getElementById("tempo_total")
 
 music.addEventListener('play', play_evento, false);
 music.addEventListener('timeupdate', atualizar, false);
-music.addEventListener('click', AttBarra, false);
 
-timer = setInterval(range_slider, 1000)
+
 
 function play_evento() {
 
     tempoAtual.innerHTML = secToStr(music.currentTime)
     tempoTotal.innerHTML = secToStr(music.duration)
 
-    
-    ponto.max = music.duration;
     barra.max = music.duration;
-    barra.value = music.currentTime;
+   
 }
 
 function atualizar() {
@@ -46,13 +44,16 @@ function secToStr(sec_num) {
     return tempo
 }
 
-function AttBarra(){
-   barra.width = music.duration
-   barra.width = music.currentTime
 
-    barra.max = music.duration;
-    barra.value = music.currentTime;
-}
+document.getElementById("barra").addEventListener('click', (e)=>{ 
+let tamanhoBarra = e.srcElement.clientWidth;
+let clickBar = e.offsetX;
+let value = (100 * clickBar) / tamanhoBarra;
+
+let duration = music.duration;
+let current = (duration / 100) * value;
+music.currentTime = current;
+})
 
 function pause() {
     music.play();
