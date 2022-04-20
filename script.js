ee389a534062ed5569e4a537224729c9dbb003bd
Gust4v0M <gustavomorgado6@gmail.com>
2022-04-20 00:20:32 -0300

    done, i guess

--- script.js
+++ script.js
@@ -2,21 +2,29 @@
 var i = 0
 var musicas = [
    
-        {mp3:"./audio/All-I-Want.mp3", nome:'All I Want', src:"./audio/All-I-Want.mp3"},     
-        {mp3:"./audio/its_you.mpeg", nome:'Its you', src:"./audio/its_you.mpeg"},
-        {mp3:"./audio/Say_You_Won't_Let_Go.mp3", nome:'Say you wont let go', src:"./audio/Say_You_Won't_Let_Go.mp3"},
+        {mp3:"./audio/All-I-Want.mp3", nome:'All I Want', src:"./audio/All-I-Want.mp3", img:'./capas/sad_mikey.jpg'},     
+        {mp3:"./audio/its_you.mpeg", nome:'Its you', src:"./audio/its_you.mpeg",  img:'./capas/its_you.jpg'},
+        {mp3:"./audio/Say_You_Won't_Let_Go.mp3", nome:'Say you wont let go', src:"./audio/Say_You_Won't_Let_Go.mp3",  img:'./capas/James_arthur.jpg'},
     ]
 
 let music = document.getElementById("music")
 let barra = document.getElementById("barra")
-
+let nextMusic = document.getElementById("nextMusic")
+let volumeIcon = document.getElementById("volume")
+let volumeBar = document.getElementById("volumeBar")
+let Play = document.getElementById("play")
+let Pause = document.getElementById("pause")
 
 let tempoAtual = document.getElementById("tempo_atual")
 let tempoTotal = document.getElementById("tempo_total")
 
-music.addEventListener('ended', proxima, false)
-music.addEventListener('play', play_evento, false);
+music.addEventListener('canplay', play_evento, false);
 music.addEventListener('timeupdate', atualizar, false);
+music.addEventListener('ended', renderizarMusica, false)
+music.addEventListener('ended', voltarMusica, false)
+
+
+
 
 
 function play_evento() {
@@ -34,16 +42,32 @@ function atualizar() {
     barra.value = music.currentTime;
 }
 
-function proxima(){
+function renderizarMusica(){
     if(music.canPlayType("audio/mp3") != ''){
         music.src = musicas[i].mp3;
     }
     document.getElementById('nome_musica').innerHTML = musicas[i].nome;
+    document.getElementById('capa2').src = musicas[i].img;
     music.play();
     
     i++
     if( i >= musicas.length) i = 0;
+    
     }
+
+    function voltarMusica (){
+        if(music.canPlayType("audio/mp3") != ''){
+            music.src = musicas[i].mp3;
+        }
+        document.getElementById('nome_musica').innerHTML = musicas[i].nome;
+        document.getElementById('capa2').src = musicas[i].img;
+        music.play();
+        
+        i--
+        if( i >= musicas.length) i = -0;
+    }
+
+
 
 function secToStr(sec_num) {
     sec_num = Math.floor(sec_num)
@@ -71,8 +95,16 @@ music.currentTime = current;
 })
 
 
-function pause() {
+function tocarMusica(){
     music.play();
+    Pause.style.display ="inline-block";
+    Play.style.display ="none";
+}
+
+function pausarMusica(){
+    music.pause();
+    Pause.style.display ="none";
+    Play.style.display ="inline-block";
 }
 
 function volta() {
@@ -82,7 +114,14 @@ function volta() {
 function avanca() {
     music.currentTime += 5
 }
+function barraa(){
+music.volume = volumeBar.value
+}
 
-function aumentaVol() {
-    music.volume
+function dentro() {
+    music.innerHTML = volumeBar.style.display = "block"
+    music.volume = volumeBar.volume
+}
+function fora(){
+    music.innerHTML = volumeBar.style.display = "none"
 }
